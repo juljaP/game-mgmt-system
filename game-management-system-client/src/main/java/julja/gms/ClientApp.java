0f639e4dbd5eb84a392177e3b6890b841671adc3
juljasong <juljasong@gmail.com>
2020-02-22 17:53:41 +0900

    0222

--- game-management-system-client/src/main/java/julja/gms/ClientApp.java
+++ game-management-system-client/src/main/java/julja/gms/ClientApp.java
@@ -1,94 +1,27 @@
 package julja.gms;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-import julja.gms.dao.BoardDao;
-import julja.gms.dao.GameDao;
-import julja.gms.dao.UserDao;
-import julja.gms.dao.mariadb.BoardDaoImpl;
-import julja.gms.dao.mariadb.GameDaoImpl;
-import julja.gms.dao.mariadb.UserDaoImpl;
-import julja.gms.handler.BoardAddCommand;
-import julja.gms.handler.BoardDeleteCommand;
-import julja.gms.handler.BoardDetailCommand;
-import julja.gms.handler.BoardListCommand;
-import julja.gms.handler.BoardUpdateCommand;
-import julja.gms.handler.Command;
-import julja.gms.handler.GameAddCommand;
-import julja.gms.handler.GameDeleteCommand;
-import julja.gms.handler.GameDetailCommand;
-import julja.gms.handler.GameListCommand;
-import julja.gms.handler.GameUpdateCommand;
-import julja.gms.handler.UserAddCommand;
-import julja.gms.handler.UserDeleteCommand;
-import julja.gms.handler.UserDetailCommand;
-import julja.gms.handler.UserListCommand;
-import julja.gms.handler.UserUpdateCommand;
 import julja.util.Prompt;
 
 public class ClientApp {
 
   Scanner sc = new Scanner(System.in);
   Prompt prompt = new Prompt(sc);
+
   Deque<String> commandStack;
   Queue<String> commandQueue;
-  Connection con;
-  HashMap<String, Command> commandMap;
-  String host;
-  int port;
+
 
   public ClientApp() throws Exception {
     commandStack = new ArrayDeque<>();
     commandQueue = new LinkedList<>();
-
-    Class.forName("org.mariadb.jdbc.Driver");
-    con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/gmsdb", "study", "1111");
-
-    BoardDao boardDao = new BoardDaoImpl(con);
-    UserDao userDao = new UserDaoImpl(con);
-    GameDao gameDao = new GameDaoImpl(con);
-    commandMap = new HashMap<>();
-
-    commandMap.put("/board/list", new BoardListCommand(boardDao));
-    commandMap.put("/board/add", new BoardAddCommand(boardDao, prompt));
-    commandMap.put("/board/detail", new BoardDetailCommand(boardDao, prompt));
-    commandMap.put("/board/update", new BoardUpdateCommand(boardDao, prompt));
-    commandMap.put("/board/delete", new BoardDeleteCommand(boardDao, prompt));
-
-    commandMap.put("/game/list", new GameListCommand(gameDao));
-    commandMap.put("/game/add", new GameAddCommand(gameDao, prompt));
-    commandMap.put("/game/detail", new GameDetailCommand(gameDao, prompt));
-    commandMap.put("/game/update", new GameUpdateCommand(gameDao, prompt));
-    commandMap.put("/game/delete", new GameDeleteCommand(gameDao, prompt));
-
-    commandMap.put("/user/list", new UserListCommand(userDao));
-    commandMap.put("/user/add", new UserAddCommand(userDao, prompt));
-    commandMap.put("/user/detail", new UserDetailCommand(userDao, prompt));
-    commandMap.put("/user/update", new UserUpdateCommand(userDao, prompt));
-    commandMap.put("/user/delete", new UserDeleteCommand(userDao, prompt));
-
-    commandMap.put("/server/stop", () -> {
-      try (Socket socket = new Socket(host, port);
-          ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-          ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
-        out.writeUTF("/server/stop");
-        out.flush();
-        System.out.println("서버: " + in.readUTF());
-        System.out.println("안녕!");
-      } catch (Exception e) {
-
-      }
-    });
   }
 
   public void service() {
@@ -118,17 +51,51 @@ public class ClientApp {
   }
 
   private void processCommand(String command) {
+    String host = null;
+    int port = 9999;
+    String servletPath = null;
 
-    // System.out.println("서버와 연결되었습니다.");
-    Command commandHandler = commandMap.get(command);
+    // ex) bitcamp://localhost:9999/board/list
+    try {
+      if (!command.startsWith("bitcamp://")) {
+        throw new Exception("명령어 형식이 옳지 않습니다.");
+      }
 
-    if (commandHandler == null) {
-      System.out.println("실행할 수 없는 명령입니다.");
+      String url = command.substring(10);
+      System.out.println(url);
+
+      int index = url.indexOf('/');
+      String[] str = url.substring(0, index).split(":");
+      host = str[0];
+
+      if (str.length == 2) {
+        port = Integer.parseInt(str[1]);
+      }
+      servletPath = url.substring(index);
+      System.out.printf("=> %s\n", servletPath);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
       return;
     }
 
-    commandHandler.execute();
-    // System.out.println("서버와 연결을 끊었음!");
+    // Connect to Server
+    try (Socket socket = new Socket(host, port);
+        PrintStream out = new PrintStream(socket.getOutputStream());
+        Scanner in = new Scanner(socket.getInputStream())) {
+
+      out.println(servletPath);
+      out.flush();
+
+      while (true) {
+        String response = in.nextLine();
+        if (response.equals("!end!")) {
+          break;
+        }
+        System.out.println(response);
+      }
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
   }
 
   private void printCommandHistory(Iterator<String> iter) {
